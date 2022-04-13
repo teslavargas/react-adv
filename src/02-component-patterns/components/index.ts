416a8d4993a30dddce1bc17c6ce9c6a981802f4d
Joel <teslavargas18@gmail.com>
2022-04-13 17:06:58 -0300

    Component Compound listo

---
+++ src/02-component-patterns/components/index.ts
@@ -0,0 +1,18 @@
+import { ProductCard as ProductCardHOC} from "./ProductCard";
+import { ProductCardHocProps } from "../interfaces/interfaces";
+
+import { ProductButtons } from "./ProductButtons";
+import { ProductImage } from "./ProductImage";
+import { ProductTitle } from "./ProductTitle";
+
+export { ProductButtons } from "./ProductButtons";
+export { ProductImage } from "./ProductImage";
+export { ProductTitle } from "./ProductTitle";
+
+export const ProductCard: ProductCardHocProps = Object.assign(ProductCardHOC, {
+    Title: ProductTitle,
+    Image: ProductImage,
+    Buttons: ProductButtons
+})
+
+export default ProductCard; 
--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -7,6 +7,8 @@ import {
 } from "react-router-dom";
 import logo from "../logo.svg";
 
+import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage";
+
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -19,7 +21,7 @@ export const Navigation = () => {
                 to="/home"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Home
+                Shopping
               </NavLink>
             </li>
             <li>
@@ -42,7 +44,7 @@ export const Navigation = () => {
         </nav>
 
         <Routes>
-          <Route path="home" element={<h1>Home Page</h1>} />
+          <Route path="home" element={<ShoppingPage />} />
           <Route path="about" element={<h1>About Page</h1>} />
           <Route path="users" element={<h1>Users Page</h1>} />
 
