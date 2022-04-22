063afa78218a6f0e2a63992b066b7dac43639137
Joel <teslavargas18@gmail.com>
2022-04-22 13:25:46 -0300

    Control Props Alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -13,23 +13,16 @@ export const useShoppingCart = () => {
     count: number;
     product: Product;
   }) => {
-    setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+    setShoppingCart((oldShoppingCart) => { 
+      if( count === 0 ) {
+      const {  [product.id]: toDelete, ...rest  } = oldShoppingCart;
+      return rest;
       }
 
-      // Borrar el producto
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
+      return {
+          ...oldShoppingCart,
+          [ product.id ]: { ...product, count }
+      }
     });
   };
 
